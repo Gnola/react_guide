0dc19cba7f9b0ec27706f213c692feefd599bd36
G <53386620+Gnola@users.noreply.github.com>
2020-04-03 14:15:28 -0700

    conditional rendering. styling is next

--- src/App.js
+++ src/App.js
@@ -7,41 +7,91 @@ class App extends Component {
 
   state = {
     persons:[
-      { name: 'Max', age: 26},
-      { name: 'Manu', age: 28},
-      { name: 'Stephanie', age: 29},
+      { id: 'asdas', name: 'Max', age: 26},
+      { id: 'sdfse', name: 'Manu', age: 28},
+      { id: 'frgrs', name: 'Stephanie', age: 29},
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
-
-  switchNameHandler = (newName) => {
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
     this.setState({
-      persons:[
-        { name: newName, age: 26},
-        { name: 'Manu', age: 28},
-        { name: 'Stephanie', age: 27},
-      ]
+      showPersons: !doesShow
     })
   }
 
-  nameChangedHandler = (e) => {
-    this.setState({
-      persons:[
-        { name: 'Max', age: 26},
-        { name: e.target.value, age: 28},
-        { name: 'Stephanie', age: 27},
-      ]
+  // switchNameHandler = (newName) => {
+  //   this.setState({
+  //     persons:[
+  //       { name: newName, age: 26},
+  //       { name: 'Manu', age: 28},
+  //       { name: 'Stephanie', age: 27},
+  //     ]
+  //   })
+  // }
+  //
+  // nameChangedHandler = (e) => {
+  //   this.setState({
+  //     persons:[
+  //       { name: 'Max', age: 26},
+  //       { name: e.target.value, age: 28},
+  //       { name: 'Stephanie', age: 27},
+  //     ]
+  //   })
+  // }
+  nameChangedHandler = (e, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id
     })
+    const person = {...this.state.persons[personIndex]}  // create a new person with the values of the person that matched the id above
+    person.name = e.target.value; // then set the name of that new person as the value of what was typed
+    const persons = [...this.state.persons]  // then create a new array of original state array of persons
+    persons[personIndex] = person  // then add the updatedPerson into the person array at the original index
+    this.setState({
+      persons:persons
+    })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
   }
 
   render() {
+
+    const style = {
+      backgroudnColor:'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    // Vanilla JS Conditional Rendering
+    let persons = null; // default
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              changed={(e) => this.nameChangedHandler(e, person.id)}/>
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <button onClick={ () => this.switchNameHandler('Maxamilian!!') }>Switch Name</button>
-        <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person click={this.switchNameHandler} changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: racing</Person>
-        <Person click={this.switchNameHandler.bind(this, 'Max!')} name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button style={style} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
+        { persons }
       </div>
     );
   }
@@ -51,9 +101,28 @@ export default App;
 
 
 // OG HARDCODED PROPS
+// <button style={style} onClick={ () => this.switchNameHandler('Maxamilian!!') }>Switch Name</button>
 // <Person name='Max' age='26' />
 // <Person name='Manu' age='28' >My Hobbies: racing</Person>
 // <Person name='Stephanie' age='29' />
+
+
+// Ternary Rendering
+// <div className="App">
+//   <button style={style} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
+//   {
+//     this.state.showPersons ?
+//       <div>
+//         <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+//         <Person click={this.switchNameHandler} changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: racing</Person>
+//         <Person click={this.switchNameHandler.bind(this, 'Max!')} name={this.state.persons[2].name} age={this.state.persons[2].age} />
+//       </div>
+//       :
+//       null
+//   }
+// </div>
+
+
 
 
 // Using React HOOKS //
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css'; // Import CSS
+
 
 // FUNCTIONAL COMPONENT - StateLESS Component
 const person = (props) => {
   return (
-    <div>
+    <div className='Person'>
       <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name}/>
