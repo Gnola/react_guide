15813691a1d55a21efd9da84e91565fb90a352e6
G <53386620+Gnola@users.noreply.github.com>
2020-04-03 15:16:32 -0700

    styled using css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
+import classes from './App.css';
 
 // Components
 import Person from './Person/Person';
@@ -22,6 +23,7 @@ import Person from './Person/Person';
 //     color: black;
 //   }
 // `
+// <StyledButton alt={this.state.showPersons} onClick={ this.togglePersonsHandler }>Toggle Persons</StyledButton>
 
 // COMPONENT
 class App extends Component {
@@ -100,6 +102,8 @@ class App extends Component {
 
     // Vanilla JS Conditional Rendering
     let persons = null; // default
+    // let btnClass = [classes.Button]
+    let btnClass = ''
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -119,24 +123,28 @@ class App extends Component {
       //   backgroundColor: 'salmon',
       //   color:'black'
       // }
+      // btnClass.push(classes.Red)
+      btnClass = classes.Red
     }
 
     // DYNAMIC CLASSES
     // let classes = ['red', 'bold'].join(' ');
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      // assignedClasses.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <=1) {
-      classes.push('bold')
+      // assignedClasses.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi! I'm a React App</h1>
-          <p className={classes.join(' ')} >This is working</p>
-          <StyledButton alt={this.state.showPersons} onClick={ this.togglePersonsHandler }>Toggle Persons</StyledButton>
+          <p className={assignedClasses.join(' ')} >This is working</p>
+          <button className={btnClass} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
           { persons }
         </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,21 @@
 import React from 'react';
 // import './Person.css'; // Import CSS
 // import Radium from 'radium';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import classes from './Person.css';
 
 
-const StyledDiv = styled.div`
-      width: 60%;
-      margin: 16px auto;
-      border: 1px solid #eee;
-      box-shadow: 0 2px 3px #ccc;
-      padding: 16px;
-      text-align: center;
-
-      @media (min-width: 500px) {
-        width: 450px;
-      }`
+// const StyledDiv = styled.div`
+//       width: 60%;
+//       margin: 16px auto;
+//       border: 1px solid #eee;
+//       box-shadow: 0 2px 3px #ccc;
+//       padding: 16px;
+//       text-align: center;
+//
+//       @media (min-width: 500px) {
+//         width: 450px;
+//       }`
 
 
 // FUNCTIONAL COMPONENT - StateLESS Component
@@ -26,11 +27,11 @@ const person = (props) => {
   // }
   return (
     // <div className='Person' style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name}/>
-    </StyledDiv>
+    </div>
   )
 }
 
