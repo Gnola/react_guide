ac32daff83b9a59cbd34cbf6e289cc7189e1a60c
G <53386620+Gnola@users.noreply.github.com>
2020-04-08 13:46:47 -0700

    cleaned up old code and moved to notes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 
-// COMPONENTS
-  import Person from './Person/Person';
+// COMPONENTS //
+import Person from './Person/Person';
 
-// STYLING
-  // import './App.css'; // Using OG CSS
-  // import Radium, { StyleRoot } from 'radium'; // Using RADIUM
-  // import styled from 'styled-components'; // Using STYLED COMPONENTS
-  import classes from './App.css'; // Using CSS CLASSES
-
-    // STYLED COMPONENTS
-    // const StyledButton = styled.button`
-    //   background-color: ${props => props.alt ? 'red' : 'green'};
-    //   color: white;
-    //   font: inherit;
-    //   border: 1px solid blue;
-    //   padding: 8px;
-    //   cursor: pointer;
-    //
-    //   &:hover{
-    //     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    //     color: black;
-    //   }
-    // `
-    // <StyledButton alt={this.state.showPersons} onClick={ this.togglePersonsHandler }>Toggle Persons</StyledButton>
+// STYLING //
+// import './App.css'; // Using OG CSS
+import classes from './App.css'; // Using CSS CLASSES
 
 
 // CLASS COMPONENT //
@@ -38,77 +20,35 @@ class App extends Component {
     showPersons: false
   }
 
-  // Show Persons
+  // TOGGLE ALL PEOPLE
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
-    })
+    this.setState({ showPersons: !doesShow })
   }
 
-  // switchNameHandler = (newName) => {
-  //   this.setState({
-  //     persons:[
-  //       { name: newName, age: 26},
-  //       { name: 'Manu', age: 28},
-  //       { name: 'Stephanie', age: 27},
-  //     ]
-  //   })
-  // }
-  //
-  // OG nameChangedHandler
-  // nameChangedHandler = (e) => {
-  //   this.setState({
-  //     persons:[
-  //       { name: 'Max', age: 26},
-  //       { name: e.target.value, age: 28},
-  //       { name: 'Stephanie', age: 27},
-  //     ]
-  //   })
-  // }
-
+  // CHANGE NAME
   nameChangedHandler = (e, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id
-    })
-    const person = {...this.state.persons[personIndex]}  // create a new person with the values of the person that matched the id above
+    const personIndex = this.state.persons.findIndex((p) => { return p.id === id}) // find person clicked on by passing in their id
+    const person = {...this.state.persons[personIndex]}  // create a new person object with the values of the person that matched the id above
     person.name = e.target.value; // then set the name of that new person as the value of what was typed
     const persons = [...this.state.persons]  // then create a new array of original state array of persons
     persons[personIndex] = person  // then add the updatedPerson into the person array at the original index
-    this.setState({
-      persons:persons
-    })
+    this.setState({ persons:persons }) // then set state
   }
 
-  // DELETE People
+  // DELETE PERSON
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons]
-    persons.splice(personIndex, 1);
-    this.setState({persons:persons});
+    const persons = [...this.state.persons] // create new array from state
+    persons.splice(personIndex, 1); // manipulate that new array
+    this.setState({persons:persons}); // then set state with new array
   }
 
   render() {
 
-    // OG Syle for button
-    // const style = {
-    //   backgroundColor:'green',
-    //   color:'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover':{
-    //     backgroundColor: 'lightgreen',
-    //     color:'black'
-    //   }
-    // }
-    // <button style={style} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
-
-    // Vanilla JS Conditional Rendering
+    // Conditional Rendering w/ Vanilla JS
     let persons = null; // default
-    // let btnClass = [classes.Button]
     let btnClass = '' // Using CSS CLASSES
+
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -122,107 +62,28 @@ class App extends Component {
           })}
         </div>
       );
-      // OG conitional button CSS
-      // style.backgroundColor = 'red' // dynamically set CSS of button
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color:'black'
-      // }
-      // btnClass.push(classes.Red)
       btnClass = classes.Red // Using CSS CLASSES
     }
 
     // DYNAMIC CLASSES
-    // let classes = ['red', 'bold'].join(' ');
     const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      // assignedClasses.push('red')
       assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <=1) {
-      // assignedClasses.push('bold')
       assignedClasses.push(classes.bold)
     }
 
 
     return (
-        <div className={classes.App}>
-          <h1>Hi! I'm a React App</h1>
-          <p className={assignedClasses.join(' ')} >This is working</p>
-          <button className={btnClass} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
-          { persons }
-        </div>
+      <div className={classes.App}>
+        <h1>Hi! I'm a React App</h1>
+        <p className={assignedClasses.join(' ')} >This is working</p>
+        <button className={btnClass} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
+        { persons }
+      </div>
     );
   }
 }
 
 export default App;
-// export default Radium(App);
-
-
-// OG HARDCODED PROPS
-// <button style={style} onClick={ () => this.switchNameHandler('Maxamilian!!') }>Switch Name</button>
-// <Person name='Max' age='26' />
-// <Person name='Manu' age='28' >My Hobbies: racing</Person>
-// <Person name='Stephanie' age='29' />
-
-
-// Ternary Rendering
-// <div className="App">
-//   <button style={style} onClick={ this.togglePersonsHandler }>Toggle Persons</button>
-//   {
-//     this.state.showPersons ?
-//       <div>
-//         <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age} />
-//         <Person click={this.switchNameHandler} changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: racing</Person>
-//         <Person click={this.switchNameHandler.bind(this, 'Max!')} name={this.state.persons[2].name} age={this.state.persons[2].age} />
-//       </div>
-//       :
-//       null
-//   }
-// </div>
-
-
-
-
-// Using React HOOKS //
-// import React, { useState } from 'react'; // HOOKS
-//
-// const app = (props) => {
-//
-//   // useState - Allows us to use state managment to functional components - can have as many as you want/need
-//   const [ personsState, setPersonsState ] = useState({ // DESTRUCTURING -
-//       persons:[
-//         { name: 'Max', age: 26},
-//         { name: 'Manu', age: 28},
-//         { name: 'Stephanie', age: 29},
-//       ],
-//       otherState: 'some other value'
-//     });
-//
-//   const [ otherState, setOtherState ] = useState('some other value')
-//   console.log(personsState, otherState);
-//
-//
-//   const switchNameHandler = () => {
-//     setPersonsState({
-//       persons:[
-//         { name: 'Maxamilian', age: 26},
-//         { name: 'Manu', age: 28},
-//         { name: 'Stephanie', age: 27},
-//       ],
-//       otherState:personsState.otherState // have to manually merge states
-//     })
-//   }
-//
-//   return (
-//     <div className="App">
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: racing</Person>
-//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-//     </div>
-//   );
-// }
-//
-// export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,35 +1,13 @@
 import React from 'react';
 
 // STYLING
-  // import './Person.css'; // Using OG CSS
-  // import Radium from 'radium'; // Using RADIUM
-  // import styled from 'styled-components'; // Using STYLED COMPONENTS
-  import classes from './Person.css'; // Using CSS CLASSES
+// import './Person.css'; // Using OG CSS
+import classes from './Person.css'; // Using CSS CLASSES
 
 
-  // STYLED COMPONENTS
-  // const StyledDiv = styled.div`
-  //   width: 60%;
-  //   margin: 16px auto;
-  //   border: 1px solid #eee;
-  //   box-shadow: 0 2px 3px #ccc;
-  //   padding: 16px;
-  //   text-align: center;
-  //
-  //   @media (min-width: 500px) {
-  //     width: 450px;
-  // }`
-
-
-// FUNCTIONAL COMPONENT - StateLESS Component //
+// FUNCTIONAL COMPONENT //
 const person = (props) => {
-  // const style = {
-  //   '@media (min-width: 500px)':{
-  //     width: '450px'
-  //   }
-  // }
   return (
-    // <div className='Person' style={style}>
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
       <p>{props.children}</p>
@@ -40,18 +18,3 @@ const person = (props) => {
 
 
 export default person;
-// export default Radium(person);
-
-
-// class Person extends Component {
-//   render() {
-//     return (
-//         <div>
-//           return <p>My name is {this.props}</p>
-//         </div>
-//     );
-//   }
-// }
-
-// {props.name} - Accesses actual props
-// <p>{props.children}</p>  - Accesses whatever is BETWEEN HTML tags
